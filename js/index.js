7223e52bf7eb8fc682e9bbac43b34783d7e3d84c
sjtu-yx <1783102600@qq.com>
2022-05-07 08:43:11 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,104 @@
+window.addEventListener('load', () => {
+    // 导入相关变量
+    const pen = document.querySelector('.pen-img');
+    const pen6 = document.querySelector('.pen6');
+    const pen12 = document.querySelector('.pen12');
+    const pen18 = document.querySelector('.pen18');
+    const penColor = document.querySelector('.pen-color');
+    const eraser = document.querySelector('.eraser-img');
+    const eraser10 = document.querySelector('.eraser10');
+    const eraser20 = document.querySelector('.eraser20');
+    const eraser30 = document.querySelector('.eraser30');
+    const clear = document.querySelector('.clear');
+    const drawing = document.querySelector('#drawing');
+    const ctx = drawing.getContext('2d');
+    let currentPen = 'pen6';
+    let penSize = 1;
+    //监听鼠标在画板处按下
+    drawing.onmousedown =  function (event) {
+        //初始化设置
+        ctx.strokeStyle = penColor.value;
+        ctx.lineWidth = penSize;
+        ctx.lineCap = "round"; //划线起始点圆形
+        ctx.lineJoin = "round"; //划线拐点圆角
+        if(currentPen === 'pen6' || currentPen === 'pen12' || currentPen === 'pen18') {
+            ctx.globalCompositeOperation = "source-over";
+        } else {
+            ctx.globalCompositeOperation = "destination-out";
+        }
+        let x = event.pageX - drawing.offsetLeft;
+        let y = event.pageY - drawing.offsetTop;
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        drawing.onmousemove = function (event) {
+            let x = event.pageX - drawing.offsetLeft;
+            let y = event.pageY - drawing.offsetTop;
+            ctx.lineTo(x, y);
+            ctx.stroke();
+        };
+        document.onmouseup = function () {
+            drawing.onmousemove = null;
+        };
+
+    }
+    //监听画笔颜色变化
+    penColor.addEventListener('change', () => {
+        pen6.style.backgroundColor = penColor.value;
+        pen12.style.backgroundColor = penColor.value;
+        pen18.style.backgroundColor = penColor.value;
+    })
+    //监听画笔的粗细
+    pen.addEventListener('click', () => {
+        currentPen = 'pen6';
+        penSize = 1;
+        drawing.classList.add('choose-pen')
+        drawing.classList.remove('choose-eraser')
+    })
+    pen6.addEventListener('click', () => {
+        currentPen = 'pen6';
+        penSize = 1;
+        drawing.classList.add('choose-pen')
+        drawing.classList.remove('choose-eraser')
+    })
+    pen12.addEventListener('click', () => {
+        currentPen = 'pen12';
+        penSize = 6;
+        drawing.classList.add('choose-pen')
+        drawing.classList.remove('choose-eraser')
+    })
+    pen18.addEventListener('click', () => {
+        currentPen = 'pen18';
+        penSize = 12;
+        drawing.classList.add('choose-pen')
+        drawing.classList.remove('choose-eraser')
+    })
+    //监听橡皮的大小
+    eraser.addEventListener('click', () => {
+        currentPen = 'eraser10';
+        penSize = 10;
+        drawing.classList.add('choose-eraser')
+        drawing.classList.remove('choose-pen')
+    })
+    eraser10.addEventListener('click', () => {
+        currentPen = 'eraser10';
+        penSize = 10;
+        drawing.classList.add('choose-eraser')
+        drawing.classList.remove('choose-pen')
+    })
+    eraser20.addEventListener('click', () => {
+        currentPen = 'eraser20';
+        penSize = 20;
+        drawing.classList.add('choose-eraser')
+        drawing.classList.remove('choose-pen')
+    })
+    eraser30.addEventListener('click', () => {
+        currentPen = 'eraser30';
+        penSize = 30;
+        drawing.classList.add('choose-eraser')
+        drawing.classList.remove('choose-pen')
+    })
+    //清空画板按钮
+    clear.addEventListener('click', () => {
+        drawing.height = drawing.height;
+    })
+})
